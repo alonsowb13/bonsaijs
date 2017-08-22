a36b9243565c3f21b4bcdd945ab8bcd0bd28dabf
Alonso Salcido <alonsosalcido13@gmail.com>
2017-08-21 22:42:40 -0600

    bonsai adding more items to the stage creating play function and animating objects

--- bonsai/movie.js
+++ bonsai/movie.js
@@ -1,7 +1,16 @@
-var animation = new Animation('1s', {
+/*var animation = new Animation('1s', {
     x: 190
 });
 new Rect(10, 10, 100, 100)
 .addTo(stage)
 .attr('fillColor', 'red')
-.animate(animation);
+.animate(animation);*/
+
+var myShapeA = new Rect(0,0,50,50).addTo(stage).attr('fillColor', 'red');
+var myShapeB = new Rect(0,50,50,50).addTo(stage).attr('fillColor', 'blue');
+var myAnimation = new Animation('1s', {
+    x: 100,
+    fillColor: 'green'
+});
+myAnimation.addSubjects([myShapeA, myShapeB]);
+myAnimation.play([myShapeA, myShapeB]);
