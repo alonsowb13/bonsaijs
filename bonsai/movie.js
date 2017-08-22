dab22d6d4eb377ff2878222e5586b2ba28724342
Alonso Salcido <alonsosalcido13@gmail.com>
2017-08-21 22:34:58 -0600

    bonsai refactor animation as a variable object

--- bonsai/movie.js
+++ bonsai/movie.js
@@ -1,10 +1,7 @@
+var animation = new Animation('1s', {
+    x: 190
+});
 new Rect(10, 10, 100, 100)
 .addTo(stage)
 .attr('fillColor', 'red')
-.animate('650ms',{
-    x: 100,
-    y: 500
-}, {
-    easing: 'bounceIn',
-    delay: '200ms'
-})
+.animate(animation);
