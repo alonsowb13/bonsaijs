ba81e1f3d18a62a5173fd7e3f97abafd98493392
Alonso Salcido <alonsosalcido13@gmail.com>
2017-08-21 22:46:27 -0600

    bonsai adding keyframe animation

--- bonsai/movie.js
+++ bonsai/movie.js
@@ -6,11 +6,26 @@ new Rect(10, 10, 100, 100)
 .attr('fillColor', 'red')
 .animate(animation);*/
 
-var myShapeA = new Rect(0,0,50,50).addTo(stage).attr('fillColor', 'red');
+/*var myShapeA = new Rect(0,0,50,50).addTo(stage).attr('fillColor', 'red');
 var myShapeB = new Rect(0,50,50,50).addTo(stage).attr('fillColor', 'blue');
 var myAnimation = new Animation('1s', {
     x: 100,
     fillColor: 'green'
 });
 myAnimation.addSubjects([myShapeA, myShapeB]);
-myAnimation.play([myShapeA, myShapeB]);
+myAnimation.play([myShapeA, myShapeB]);*/
+
+var thing = new Rect(0,0,50,50).addTo(stage).attr('fillColor', 'red');
+thing.attr(0, 0)
+.animate('1s', {
+    x: 100,
+    y: 100
+}, {
+    onEnd: function(){
+        thing.animate('1s', {
+            x: 0,
+            y: 0
+        })
+    }
+})
+
